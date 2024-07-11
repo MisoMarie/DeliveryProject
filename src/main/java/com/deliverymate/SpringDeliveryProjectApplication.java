733db25cb3b5f9ac694c6deaa6b82ff38239b5af
tnwls0297 <tnwls0297@naver.com>
2024-07-11 21:50:38 +0900

    commit

---
+++ src/main/java/com/deliverymate/SpringDeliveryProjectApplication.java
@@ -0,0 +1,13 @@
+package com.deliverymate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDeliveryProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDeliveryProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deliverymate/SpringDeliveryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliverymate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDeliveryProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
