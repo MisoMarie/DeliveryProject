482a6bb9b38e44f0d705a08672212cf1f5c26322
MisoMarie <148054720+MisoMarie@users.noreply.github.com>
2024-07-18 21:52:31 +0900

    d

---
+++ src/main/java/com/deliverymate/domain/ImageDTO.java
@@ -0,0 +1,39 @@
+package com.deliverymate.domain;
+
+import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Base64;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString(exclude = {"image", "imageURL", "file"})
+@NoArgsConstructor
+@AllArgsConstructor
+public class ImageDTO {
+    private Integer no;
+    private MultipartFile file;
+    private byte[] image;
+    private String imageURL;
+
+    public void setFile(MultipartFile file) {
+        this.file = file;
+        try {
+            if(Objects.nonNull(file)){
+                this.image = file.getBytes();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getImageURL() {
+        if(imageURL == null) {
+            String url = Base64.getEncoder().encodeToString(this.image);
+            this.imageURL = ("data:image/*;base64, " + url);
+        }
+        return this.imageURL;
+    }
+}
