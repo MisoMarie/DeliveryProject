34a2124ea6d85faacab2d728c1d3bb24c6e571c8
wsw8186 <131680941+wsw8186@users.noreply.github.com>
2024-07-23 18:51:10 +0900

    0723
    gd

--- src/main/java/com/deliverymate/domain/ImageDTO.java
+++ src/main/java/com/deliverymate/domain/ImageDTO.java
@@ -9,7 +9,7 @@ import java.util.Objects;
 
 @Getter
 @Setter
-@ToString(exclude = {"image", "imageURL", "file"})
+@ToString(exclude = {"image", "imageURL", "file", "storeImg", "foodImage"})
 @NoArgsConstructor
 @AllArgsConstructor
 public class ImageDTO {
@@ -17,12 +17,16 @@ public class ImageDTO {
     private MultipartFile file;
     private byte[] image;
     private String imageURL;
+    private String storeImg;
+    private String foodImage;
+
 
     public void setFile(MultipartFile file) {
         this.file = file;
         try {
-            if(Objects.nonNull(file)) {
+            if (Objects.nonNull(file)) {
                 this.image = file.getBytes();
+                setImage(this.image); // 이미지 설정 시 자동으로 imageURL과 storeImg 설정
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -31,7 +35,15 @@ public class ImageDTO {
 
     public void setImage(byte[] image) {
         this.image = image;
-        String url = Base64.getEncoder().encodeToString(this.image);
-        this.imageURL = ("data:image/*;base64, " + url);
+        if (Objects.nonNull(image)) {
+            String base64Image = Base64.getEncoder().encodeToString(this.image);
+            this.imageURL = "data:image/*;base64," + base64Image;
+            this.storeImg = "data:image/*;base64," + base64Image;
+            this.foodImage = "data:image/*;base64," + base64Image;
+        } else {
+            this.imageURL = null;
+            this.storeImg = null;
+            this.foodImage = null;
+        }
     }
 }
---
+++ src/main/resources/static/js/public/header.js
@@ -0,0 +1,30 @@
+// 로그아웃 버튼을 찾기
+const logoutBtn = document.getElementById('logout-btn');
+
+if (logoutBtn !== null) {
+    logoutBtn.onclick = (e) => {
+        e.preventDefault();
+
+        // 'header-csrf'라는 ID를 가진 폼을 찾기
+        const csrfForm = document.getElementById('header-csrf');
+        const csrfInput = csrfForm.querySelector('input[type="hidden"]');
+        const csrfName = csrfInput.name;
+        const csrfToken = csrfInput.value;
+
+        // 기존 폼을 사용하여 로그아웃 요청 처리
+        const form = document.createElement('form');
+        form.action = "/user/logout";
+        form.method = 'POST';
+
+        // CSRF 토큰을 새로운 폼에 추가
+        const csrfField = document.createElement('input');
+        csrfField.type = 'hidden';
+        csrfField.name = csrfName;
+        csrfField.value = csrfToken;
+        form.appendChild(csrfField);
+
+        // 새로운 폼을 문서 본문에 추가하고 제출
+        document.body.appendChild(form);
+        form.submit();
+    };
+}
